9fee4c7365ca9323eef92b78e5d2f0fe52a3891c
felipesmn <felipesmn@al.insper.edu.br>
2016-05-09 11:21:24 -0300

    Identifiquei todas as def

--- PETinderFlask.py
+++ PETinderFlask.py
@@ -36,9 +36,17 @@ caesdoar = {}
 
 app = Flask(__name__, static_url_path='')
 
-@app.route('/home')
-def home():
-    return render_template('home.html')
+@app.route('/')
+def fistpage():
+    return render_template('1.html')
+    
+@app.route('/login')
+def conta():
+    nomepessoa = request.args['nomepessoa']
+    email = request.args['email']
+    senha = request.args['senha']
+    
+    return render_template('login.html', dic = pessoas, erro = '')
     
 @app.route('/cadastro')
 def cadastro():
@@ -52,18 +60,46 @@ def cadastro():
     
     return render_template('cadastro.html', dic = caesexo, erro = '')
     
+@app.route('/caddoar')
+def caddoar():
+    nome = request.args['nome']
+    raca = request.args['raca']
+    sexo = request.args['sexo']
+    idade = request.args['idade']
+    cor = request.args['cor']
+    saude = request.args['saude']
+    cidade = request.args['cidade']
+    
+    return render_template('caddoar.html', dic = caesdoar, erro = '')
+    
+@app.route('/home')
+def home():
+    return render_template('home.html')
+    
 @app.route('/perfil')
+def perfil():
+    return render_template('perfil.html')
+    
+@app.route('/opt')
+def opt():
+    return render_template('opt.html')
+    
+@app.route('/opt/user')
+def user():
+    return render_template('user.html')
+    
+@app.route('/doar')
+def doar():
+    return render_template('doar.html')
+    
+@app.route('/adotar')
+def adotar():
+    return render_template('adotar.html')
+    
+@app.route('/adotar/adote')
+def adote():
+    return render_template('adote.html')
 
-    
-        
-@app.route('/login')
-def conta():
-    nomepessoa = request.args['nomepessoa']
-    email = request.args['email']
-    senha = request.args['senha']
-    
-    return render_template('login.html', dic = pessoas, erro = '')
-    
     
     
     
