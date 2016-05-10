42b0a98e672b07b6c53138a98472adcb9aa49555
eduardotp1 <edu.tirta@gmail.com>
2016-05-10 15:33:45 -0300

    firecall pt 2

--- Firecall_petinder.py
+++ Firecall_petinder.py
@@ -13,35 +13,39 @@ caes={}
 
 class Pessoa():
     
-    def __init__(self, nomepessoa, email, senha, Caes):
+    def __init__(self, nomepessoa, email, senha):
         self.nomepessoa = nomepessoa
         self.email = email
         self.senha = senha
-        self.caes = Caes
-        self.dic={}
-    def dicionario(self):
-        dic={self.nomepessoa:[self.nomepessoa,self.email,self.senha,self.Caes]}
-        return dic
 #    def Salvar_Pessoa(self):
 
 
-    class Caes():
+class Caes():
     
-        def __init__(self, nome, sexo, raca, cor, idade, saude, cidade):
-            self.nome = nome
-            self.sexo = sexo
-            self.raca = raca
-            self.cor = cor
-            self.idade = idade
-            self.saude = saude
-            self.cidade = cidade
+    def __init__(self, nome, sexo, raca, cor, idade, saude, cidade):
+        self.nome = nome
+        self.sexo = sexo
+        self.raca = raca
+        self.cor = cor
+        self.idade = idade
+        self.saude = saude
+        self.cidade = cidade
     #    def Salvar_Caes(self):
 
-Eduardo_Tirta=Pessoa('Eduardo Tirta Prawita','edu.tirta@gmail.com','0000','Lucas')       
+Pessoa.nomepessoa=input('Nome?')
+Pessoa.email=input('Email?')
+Pessoa.senha=input('Senha?')
 
+Caes.nome = input('nome?')
+Caes.sexo = input('sexo?')
+Caes.raca = input('raca?')
+Caes.cor = input('cor?')
+Caes.idade =input('idade?')
+Caes.saude =input('saude?')
+Caes.cidade =input('cidade?')
+dicionario = {"Nome" : Pessoa.nomepessoa, "Nome de usuario" : Pessoa.nomepessoa, "Email" : Pessoa.email, "Senha" : Pessoa.senha,"Cães":Caes}
 #Lucas=Caes("Lucas","masculino","york","preto","14","castrado","mogi")
+dicionariocao={'nome':Caes.nome,'sexo':Caes.sexo,'raca':Caes.raca,'cor':Caes.cor,'idade':Caes.idade,'saude':Caes.saude,'cidade':Caes.cidade}
 
-
-PETinder.put(point="/Cadastro das pessoas",data=Pessoa)
-
+PETinder.put(point="/Pessoas",data=dicionario)
 
