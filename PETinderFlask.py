f919be2d3542acbe372c112dde5c01867876a1dd
felipesmn <felipesmn@al.insper.edu.br>
2016-05-10 16:59:05 -0300

    Adicionei as condições do login

--- PETinderFlask.py
+++ PETinderFlask.py
@@ -3,11 +3,18 @@ from flask import Flask, render_template, request, redirect, url_for
 app = Flask(__name__, static_url_path='')
 
 @app.route('/')
-def fistpage():
+def firstpage():
     email = request.args['email']
     senha = request.args['senha']
-    
-    return render_template('1.html')
+    if email in dicionario:
+        if senha in dicionario:
+            return render_template('1.html', dic = dicionario)
+        else: 
+            e = 'Senha incorreta'
+            return render_template('1.html', dic = dicionario, erro = e) 
+    else:
+        e = 'Usuário inválido'
+        return render_template('1.html', dic = dicionario, erro = e)
     
 @app.route('/login', methods=['POST','GET'])
 def conta():
@@ -16,7 +23,7 @@ def conta():
         email = request.form['email']
         senha = request.form['senha']
         pessoa = Pessoa(nomepessoa, email, senha)
-        pessoa.SalvarPessoa()
+        pessoa.Salvar_Pessoa()
     
     return render_template('login.html', erro = '')
     
@@ -31,7 +38,7 @@ def cadastro():
         saude = request.form['saude']
         cidade = request.form['cidade']
         primeiro = CaesBR(nome, raca, sexo, idade, cor, saude, cidade)
-        primeiro.SalvarCaesBR()
+        primeiro.Salvar_CaesBR()
     
     return render_template('cadastro.html', erro = '')
     
@@ -46,7 +53,7 @@ def caddoar():
         saude = request.form['saude']
         cidade = request.form['cidade']
         segundo = CaesDoar(nome, raca, sexo, idade, cor, saude, cidade)
-        segundo.SalvarCaesDoar()
+        segundo.Salvar_CaesDoar()
     
     return render_template('caddoar.html', erro = '')
     
@@ -56,26 +63,32 @@ def home():
     
 @app.route('/perfil')
 def perfil():
+    #página que mostrará os cães cadastrados pelo usuário
     return render_template('perfil.html')
     
 @app.route('/opt')
 def opt():
+    #página que mostrará as opções de cães e tem as setinhas para passar
     return render_template('opt.html')
     
 @app.route('/opt/user')
 def user():
+    #informações sobre o cão escolhido
     return render_template('user.html')
     
 @app.route('/doar')
 def doar():
+    #página que mostrará os animais cadastrados pelo usuário para doação
     return render_template('doar.html')
     
 @app.route('/adotar')
 def adotar():
+    #animais disponíveis para doação e setinhas para passar
     return render_template('adotar.html')
     
 @app.route('/adotar/adote')
 def adote():
+    #informações sobre o cão escolhido
     return render_template('adote.html')
 
     
